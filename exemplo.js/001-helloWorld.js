57cc1765cfde73327bbfdd14d5b3a1fab81d62a9
YudiNamaizawa <yudi.namaizawa@etec.sp.gov.br>
2023-02-13 22:49:02 -0300

    Aula introdução
    Criação de Variaveis, Concatenação e Tipagem.

---
+++ exemplo.js/001-helloWorld.js
@@ -0,0 +1,32 @@
+// é utilizado para comentar uma linha
+/* 
+Utilizado para comentar mais de uma linha
+*/
+console.log('Estou desenvolvendo em java-script');
+let nome = 'Yudi';
+let idade = 18;
+let trabalha = false;
+let alturaPessoa = 1.65;
+let coresFavoritas = ['preto', 'azul', 'ciano', 'roxo' ]; //array
+let endereco = {
+
+    logradouro: 'Avenida',
+    nmLog: 'Brasil',
+    num: '200-A',
+    bairro: 'Centro',
+};//objeto
+let covid = null
+let dengue;
+
+console.log(alturaPessoa);
+console.log("Nome:: " + nome)
+console.log('O '+ nome + ' tem ' + idade + ' anos.');
+console.log();
+console.log('--------------------------------------');
+console.log(typeof(nome));//tipo de variavel string
+console.log(typeof(idade));// tipo de variavel number
+console.log(typeof(trabalha));//tipo de variavel boolean
+console.log(typeof(coresFavoritas));// 
+console.log(typeof(endereco));// tipo de variavel object
+console.log(typeof(covid));//
+console.log(typeof(dengue));//tipo de variavel undefined
